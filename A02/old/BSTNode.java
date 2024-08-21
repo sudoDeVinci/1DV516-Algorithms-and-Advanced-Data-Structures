9cdbcf384f72cfc5ac2151aee2bdefc9bc11b43f
Devinci <tadj.d.cazaubon@gmail.com>
2024-08-22 00:01:03 +0200

    old is out!

--- A02/old/BSTNode.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class BSTNode<T extends Comparable<T>> extends LRNode<T, BSTNode<T>>{
-  BSTNode(T data) {
-    this.value = data;
-    this.left = null;
-    this.right = null;
-  }
-}
--- A02/old/LRNode.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public abstract class LRNode<T extends Comparable<T>, U extends LRNode<T, U>> {
-    public T value;
-    public U left;
-    public U right;
-}
